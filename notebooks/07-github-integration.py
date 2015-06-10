6573faf59947820dd010f59c147edf0cae218f65
Jonathan Whitmore <jonathan.b.whitmore@gmail.com>
2015-06-10 14:03:17 -0700

    Show Serena

--- notebooks/07-github-integration.py
+++ notebooks/07-github-integration.py
@@ -1,7 +1,14 @@
 
 # coding: utf-8
 
-# In[8]:
+# In[1]:
+
+# %install_ext https://raw.githubusercontent.com/rasbt/watermark/master/watermark.py
+get_ipython().magic(u'load_ext watermark')
+get_ipython().magic(u'watermark --githash --machine --python --packages pandas,numpy,matplotlib -u --custom_time %Y-%m-%d')
+
+
+# In[2]:
 
 import pandas as pd
 import numpy as np
@@ -11,22 +18,22 @@ import seaborn as sns
 sns.set();
 
 
-# In[5]:
+# In[3]:
 
 x = np.random.random_sample(100) * 50.0
 
 
-# In[6]:
+# In[4]:
 
 y = np.sin(x)
 
 
-# In[9]:
+# In[5]:
 
 plt.scatter(x, y)
 
 
-# In[10]:
+# In[6]:
 
 plt.plot(x,y)
 
